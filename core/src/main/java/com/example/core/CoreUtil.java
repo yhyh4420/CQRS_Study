8ee3cc4906ea81d7ae9784acf68f3d459ffcc508
Yehun <yhyh4420@naver.com>
2025-10-12 18:14:38 +0900

    refactor: 불필요 패키지 제거

--- core/src/main/java/com/example/core/CoreUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.core;
-
-import org.apache.commons.lang3.StringUtils;
-
-public class CoreUtil {
-    public static String getCoreMessage() {
-        return StringUtils.reverse("Hello from CoreUtil!");
-    }
-}
